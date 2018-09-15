537599e6c763390684deddff5532659c9678ee33
Shamal Siriwardana <shamalsiriwardana@Shamals-MacBook-Pro.local>
2018-09-15 18:30:22 -0400

    Full assignment minus the code commenting

--- assignment1/myScript.sh
+++ assignment1/myScript.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-ls
-./prog 32
+args=("$@")
+elements=${#args[@]}
+echo $elements
+
+if [ $elements -eq 0 ]; then
+  echo Invalid argument list.
+  echo Proper usage: myscript m1[m2 [m3 [...]]]
+else
+  for ((i=0;i<$elements;i++))
+  do
+    ./prog ${args[${i}]}
+  done
+  for ((i=0;i<$elements;i++))
+  do
+    for((j=i+1; j<$elements; j++))
+    do
+      ./prog ${args[${i}]} ${args[${j}]}
+    done
+  done
+fi
+
+
+arguments - parameters
