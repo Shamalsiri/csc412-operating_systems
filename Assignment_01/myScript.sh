f412a4850468fc6ab3f92c598adc70caf81ab5ad
Shamal Siriwardana <shamalsiriwardana@Shamals-MacBook-Pro.local>
2018-09-15 20:39:14 -0400

    rename parent folder for assignment1

---
+++ Assignment_01/myScript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+args=("$@")
+elements=${#args[@]}
+
+if [ $elements -eq 0 ]; then
+  echo Invalid argument list.
+  echo Proper usage: myscript m1[m2 [m3 [...]]]
+else
+  for ((i=0;i<$elements;i++))
+  do
+    ./prog ${args[${i}]}
+  done
+  for ((i=0;i<$elements;i++))
+  do
+    for((j=i+1; j<$elements; j++))
+    do
+      ./prog ${args[${i}]} ${args[${j}]}
+    done
+  done
+fi
+
+
