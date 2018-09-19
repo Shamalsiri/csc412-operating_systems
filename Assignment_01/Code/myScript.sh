97e665f9dc52affc3bcd9057c58e71ae0562912f
Shamal Siriwardana <shamalsiriwardana@Shamals-MacBook-Pro.local>
2018-09-18 23:19:20 -0400

    Submited Assignment

---
+++ Assignment_01/Code/myScript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+args=("$@") # store Arguments
+elements=${#args[@]} # store the number of arguments
+
+err1="Invalid argument list." #err msg 1
+err2="Proper usage: myScript <path ot executable> m1 [m2 [m3 [...]]]" #err msg 2
+
+if [ $elements -eq 0 ]; then # If there is no arguments, prints error message
+  echo $err1
+  echo $err2
+elif [ $elements -eq 1 ]; then # If the argument is only the path to executable,
+  echo $err1 # prints error message
+  echo $err2
+else # If the arguments include the path executable (exe) and list of ints
+  path="./" # set ./
+  path=$path${args[${1}]} #Path to exe
+  for ((i=1;i<$elements;i++)) # for lookp that Call the exe with indiviual ints
+  do
+    $path ${args[${i}]}
+  done
+  for ((i=1;i<$elements;i++)) #for loop that pairs up the ints
+  do
+    for((j=i+1; j<$elements; j++))
+    do
+      $path ${args[${i}]} ${args[${j}]} #calls the exe with the paired 2 ints
+    done
+  done
+fi
---
+++ Assignment_01/Code/src.c
@@ -0,0 +1,112 @@
+// Libraries included
+#include<stdio.h>
+#include<stdlib.h>
+
+// Define the helper method/function
+int gcd(int,int);
+
+/*
+* Main funciton,
+* It has two parameters argc of type int and argv a pointer to a char array
+* If there is one argument it will prints a list of divisors
+* If there is two arguments it will prints the greatest common denominator
+*/
+int main(int argc, char *argv[])
+{
+  int param1;
+  int param2;
+
+  if (argc == 1) // If no arguments, prints error message
+  {
+    printf("Proper usage: prog m [n]\n");
+  }
+  else if (argc == 2) //If there is 1 argument
+  {
+    param1 = atoi(argv[1]); // Convert char[] to an int
+
+    if (param1 < 0) // If the int is a negative number, prints error message
+    {
+      printf("prog’s argument is not a strictly positive integer.\n");
+    }
+    // If the int is 0, print the divisor as 0.
+    // Any words (non- numbers) would be considered 0 here
+    else if(param1 == 0)
+    {
+      printf("The list of divisors of %d is: %d\n", param1, param1);
+    }
+    else // If the int is a positive number, list of divisors will be printed
+    {
+      printf("The list of divisors of %d is: ", param1);
+      for(int i = 1; i <= param1; i++)
+      {
+        if(param1 % i == 0)
+        {
+          printf("%d", i);
+          if(i != param1)
+          {
+            printf(", ");
+          }
+          else
+          {
+            printf(".");
+          }
+        }
+
+      }
+      printf("\n");
+    }
+  }
+  else if (argc == 3) // If there is 2 arguments
+  {
+    param1 = atoi(argv[1]); // convert argv[1] to int
+    param2 = atoi(argv[2]); // convert argv[2] to int
+
+    if(param1 < 0) // If the first argument is negative, prints error message
+    {
+      printf("prog’s first argument is not a strictly positive integer.\n");
+    }
+    // If the second argument is negative, prints error message
+    else if(param2 < 0)
+    {
+      printf("prog’s second argument is not a strictly positive integer.\n");
+    }
+    else // If both arguments are postive, GCD is calculated
+    {
+      printf("The gcd of %d and %d is ", param1, param2);
+      // Call the helper method that calculates the gcd
+      int gcdenominator = gcd(param1, param2);
+      printf("%d.\n", gcdenominator);
+    }
+  }
+  else // If there is more than 2 arguments, prints error message 
+  {
+    printf("Proper usage: prog m [n]\n");
+  }
+
+  return 0;
+}
+
+/*
+* Helper Method (Recursive)- takes 2 ints as parameters
+* Calculate the Greated Common Denominator
+* Returns the gcd of type int
+*/
+int gcd(int p1, int p2)
+{
+  if(p1 == 0 || p2 == 0) // if one or both parameters are 0
+  {
+    return 0;
+  }
+  else if(p1 == p2) // if the paramters are the same
+  {
+    return p2;
+  }
+  else if(p1 > p2) // If the first parameter is larger than the second
+  {
+    return gcd(p1 - p2, p2); // recursive call to the method
+  }
+  else // If the second argument is larger than the first
+  {
+    return gcd(p1, p2 - p1); //recursive call to the method
+  }
+}
