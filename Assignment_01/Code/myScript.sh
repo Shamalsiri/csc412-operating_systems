743c62eacfb39795bc86373ac69d3d2e95095c90
Shamal Siriwardana <shamalsiriwardana@189.59.20.172.s.wireless.uri.edu>
2018-09-21 13:30:08 -0400

    Checks to see if the first argument is executable

--- Assignment_01/Code/myScript.sh
+++ Assignment_01/Code/myScript.sh
@@ -5,23 +5,25 @@ elements=${#args[@]} # store the number of arguments
 err1="Invalid argument list." #err msg 1
 err2="Proper usage: myScript <path ot executable> m1 [m2 [m3 [...]]]" #err msg 2
 
-if [ $elements -eq 0 ]; then # If there is no arguments, prints error message
-  echo $err1
-  echo $err2
-elif [ $elements -eq 1 ]; then # If the argument is only the path to executable,
+if [ $elements -lt 2 ]; then # If there is no arguments or 1 argument
   echo $err1 # prints error message
   echo $err2
 else # If the arguments include the path executable (exe) and list of ints
   path=$1
-  for ((i=1;i<$elements;i++)) # for lookp that Call the exe with indiviual ints
-  do
-    $path ${args[${i}]}
-  done
-  for ((i=1;i<$elements;i++)) #for loop that pairs up the ints
-  do
-    for((j=i+1; j<$elements; j++))
+  if [ -x "$path" ]; then # check if the path is executable
+    for ((i=1;i<$elements;i++)) # for loop that Call the exe with indiviual int
     do
-      $path ${args[${i}]} ${args[${j}]} #calls the exe with the paired 2 ints
+      $path ${args[${i}]} #calls the exe with the int
     done
-  done
+    for ((i=1;i<$elements;i++)) #for loop that pairs up the ints
+    do
+      for((j=i+1; j<$elements; j++))
+      do
+        $path ${args[${i}]} ${args[${j}]} #calls the exe with the paired 2 ints
+      done
+    done
+  else # If path isn't executable, prints error msg
+    echo $err1
+    echo $err2
+  fi
 fi
